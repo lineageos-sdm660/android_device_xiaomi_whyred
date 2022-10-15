fd6f4d8dd8dc8ed19540664639389998b4a327b5
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    whyred: extract-files: Simplify double patchelf addition avoidance
    Change-Id: I2d4d393767e9870b3eb24cf13d531d5a602dcf40

--- extract-files.sh
+++ extract-files.sh
@@ -9,9 +9,7 @@
 function blob_fixup() {
     case "${1}" in
         vendor/lib/hw/camera.sdm660.so)
-            for LIBCAMERA_SDM660_SHIM in $(grep -L "libcamera_sdm660_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "$LIBCAMERA_SDM660_SHIM"
-            done
+            grep -q "libcamera_sdm660_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "${2}"
             ;;
         vendor/lib64/libgf_ca.so)
             sed -i 's|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g' "${2}"
